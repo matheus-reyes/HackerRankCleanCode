986cadfdb46b774a74c13089cef0b919361eb01f
matheus-reyes <matheuscreyes@gmail.com>
2021-02-06 18:44:56 -0300

    BirthdayCakeCandles

---
+++ HackerRank/BirthdayCakeCandles/Main.java
@@ -0,0 +1,47 @@
+package HackerRank.BirthdayCakeCandles;
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        int numberOfCandles = input.nextInt();
+        int[] candles = getCandles(input, numberOfCandles);
+        input.close();
+        System.out.println(birthdayCakeCandles(candles));
+
+    }
+
+    public static int[] getCandles(Scanner input, int numberOfCandles){
+
+        int[] candles = new int[numberOfCandles];
+
+        for(int i = 0; i < candles.length; i++){
+            candles[i] = input.nextInt();
+        }
+
+        input.close();
+
+        return candles;
+
+    }
+
+    public static int birthdayCakeCandles(int[] candles){
+
+        int tallestCandle = 0;
+        int numberOfTallestCandles = 0;
+
+        for(int i = 0; i < candles.length; i++){
+            if(candles[i] > tallestCandle){
+                tallestCandle = candles[i];
+                numberOfTallestCandles = 1;
+            } 
+            else if(tallestCandle == candles[i]) numberOfTallestCandles++;
+        }
+
+        return numberOfTallestCandles;
+
+    }
+
+}
