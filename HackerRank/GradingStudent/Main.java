011f1219d4a1057b712644ae198ea81dff30504c
matheus-reyes <matheuscreyes@gmail.com>
2021-02-06 20:26:04 -0300

    TimeConversion and GradingStudent

---
+++ HackerRank/GradingStudent/Main.java
@@ -0,0 +1,39 @@
+package HackerRank.GradingStudent;
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        int numberOfStudents = input.nextInt();
+        int[] grades = getGrades(input, numberOfStudents);
+        gradingStudents(grades);
+        input.close();
+
+    }
+
+    public static int[] getGrades(Scanner input, int numberOfStudents){
+
+        int[] grades = new int[numberOfStudents];
+
+        for(int i = 0; i < grades.length; i++){
+            grades[i] = input.nextInt();
+        }
+
+        input.close();
+
+        return grades;
+    }
+
+    public static void gradingStudents(int[] grades){
+
+        for(int i = 0; i < grades.length; i++){
+            if((grades[i] / 5) * 5 + 5 - grades[i] < 3 && grades[i] >= 38) 
+                grades[i] = (grades[i] / 5) * 5 + 5;
+            System.out.println(grades[i]);
+        }
+
+    }
+
+}
---
+++ HackerRank/TimeConversion/Main.java
@@ -0,0 +1,28 @@
+package HackerRank.TimeConversion;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        String string = input.next();
+        input.close();
+        System.out.println(timeConversion(string));
+
+    }
+
+    public static String timeConversion(String string){
+
+        String hourFormat = string.substring(string.length() - 2, string.length());
+        int hours = Integer.parseInt(string.substring(0, 2));
+        String restOfString = string.substring(2, string.length() - 2);
+
+        if(hourFormat.equals("AM")) hours -= 12;
+        if(hourFormat.equals("PM") && hours < 12) hours += 12;
+
+        if(hours < 10) return "0" + hours + restOfString;
+        else return hours + restOfString;
+        
+    }
+}
